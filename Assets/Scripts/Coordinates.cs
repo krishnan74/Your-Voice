03f5961aa7105dae6ada37e65d8ffd9f9bd4bd15
Speckleszzzz <speckleszzzz@gmail.com>
2024-12-30 02:57:41 +0530

    letters placed

---
+++ Assets/Scripts/Coordinates.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coordinates : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3(5.5,0,192.5)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
